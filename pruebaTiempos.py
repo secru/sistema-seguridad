3ad0d5672ec0edcd940b7af861d38ba617fb7d6e
secru <cruzd9@hotmail.com>
2018-02-03 22:40:33 -0500

    Tiempos en los diferentes modos de cifrado

--- pruebaTiempos.py
+++ pruebaTiempos.py
@@ -9,92 +9,122 @@ unpad = lambda s : s[0:-ord(s[-1])]
 llave= '23212345123456789123456723212345'
 iv = '123456789abcdefg'
 
-msg = 'Prueba'
+msg = 'P'
+tmMsg = 1024 *100
+#msg=msg.zfill(tmMsg)
+print msg
 
 #MODO ECB - Bloque tamano fijo
-ti_ECB=time()
+tic_ECB=time()
 cifrador = AES.new(llave,AES.MODE_ECB)
 msgen = cifrador.encrypt(pad(msg))
+tfc_ECB=time()
+tiempocECB=tfc_ECB-tic_ECB
 #print msgen
 
-decipher = AES.new(llave,AES.MODE_ECB)		
+ti_ECB=time()
+decipher = AES.new(llave,AES.MODE_ECB)	
 unpad(decipher.decrypt(msgen))
 tf_ECB=time()
-tiempoECB=tf_ECB-ti_ECB
-print 'ECB' ,tiempoECB
+tiempodECB=tf_ECB-ti_ECB
+print 'ECB' ,'Cifrado:',tiempocECB ,'Tiempo:', (len(msg))/(tiempocECB), 'Bytes/s'
+print 'ECB' ,'Descifrado:',tiempodECB ,'Tiempo:', (len(msg))/(tiempodECB), 'Bytes/s'
 
 #MODO CFB
-ti_CFB=time()
+tic_CFB=time()
 cifrador = AES.new(llave,AES.MODE_CFB,iv)
 msgen=cifrador.encrypt(msg)
+tfc_CFB=time()
+tiempocCFB=tfc_CFB-tic_CFB
 #print msgen
 
+tid_CFB=time()
 decipher = AES.new(llave,AES.MODE_CFB,iv)		
 decipher.decrypt(msgen)
-tf_CFB=time()
-tiempoCFB=tf_CFB-ti_CFB
-print 'CFB' , tiempoCFB
-CFB =['CFB' , tiempoCFB]
+tfd_CFB=time()
+tiempodCFB=tfd_CFB-tid_CFB
+print 'CFB Cifrado' , tiempocCFB,'Tiempo:', (len(msg))/(tiempocCFB), 'Bytes/s'
+print 'CFB Descifrado' , tiempodCFB,'Tiempo:', (len(msg))/(tiempodCFB), 'Bytes/s'
 
 #MODO CBC - Bloque tamano fijo
-ti_CBC=time()
+tic_CBC=time()
 cifrador = AES.new(llave,AES.MODE_CBC,iv)
 msgen=cifrador.encrypt(pad(msg))
+tfc_CBC=time()
+tiempocCBC=tfc_CBC-tic_CBC
 #print msgen
 
+tid_CBC=time()
 decipher = AES.new(llave,AES.MODE_CBC,iv)		
 unpad(decipher.decrypt(msgen))
-tf_CBC=time()
-tiempoCBC=tf_CBC-ti_CBC
-print 'CBC' ,tiempoCBC
-CBC=['CBC' ,tiempoCBC]
+tfd_CBC=time()
+tiempodCBC=tfd_CBC-tid_CBC
+print 'CBC Cifrado' ,tiempocCBC,'Tiempo:', (len(msg))/(tiempocCBC), 'Bytes/s'
+print 'CBC Descifrado' ,tiempodCBC,'Tiempo:', (len(msg))/(tiempodCBC), 'Bytes/s'
 
 #MODO OFB - Bloque tamano fijo
-ti_OFB=time()
+tic_OFB=time()
 cifrador = AES.new(llave,AES.MODE_OFB,iv)
 msgen = cifrador.encrypt(pad(msg))
+tfc_OFB=time()
+tiempocOFB=tfc_OFB-tic_OFB
 #print msgen
 
+tid_OFB=time()
 decipher = AES.new(llave,AES.MODE_OFB,iv)		
 unpad(decipher.decrypt(msgen))
-tf_OFB=time()
-tiempoOFB=tf_OFB-ti_OFB
-print 'OFB' ,tiempoOFB
-OFB=['OFB' ,tiempoOFB]
+tfd_OFB=time()
+tiempodOFB=tfd_OFB-tid_OFB
+print 'OFB Cifrado' ,tiempocOFB,'Tiempo:', (len(msg))/(tiempocOFB), 'Bytes/s'
+print 'OFB Descifrado' ,tiempodOFB,'Tiempo:', (len(msg))/(tiempodOFB), 'Bytes/s'
 
 #MODO CTR
-ti_CTR=time()
+tic_CTR=time()
 ctr =Counter.new(128)
 cifrador = AES.new(llave,AES.MODE_CTR,counter =ctr)
 msgen=cifrador.encrypt(msg)
+tfc_CTR=time()
+tiempocCTR=tfc_CTR-tic_CTR
 #print msgen
 
+tid_CTR=time()
 decipher = AES.new(llave,AES.MODE_CTR,counter=ctr)		
 decipher.decrypt(msgen)
-tf_CTR=time()
-tiempoCTR=tf_CTR-ti_CTR
-print 'CTR' ,tiempoCTR
-CTR=['CTR' ,tiempoCTR]
+tfd_CTR=time()
+tiempodCTR=tfd_CTR-tid_CTR
+print 'CTR Cifrado' ,tiempocCTR,'Tiempo:', (len(msg))/(tiempocCTR), 'Bytes/s'
+print 'CTR Descifrado' ,tiempodCTR,'Tiempo:', (len(msg))/(tiempodCTR), 'Bytes/s'
 
 #MODO OPENPGP
-ti_OPENPGP=time()
+tic_OPENPGP=time()
 cifrador = AES.new(llave,AES.MODE_OPENPGP,iv)
 msgen=cifrador.encrypt(msg)
+tfc_OPENPGP=time()
+tiempocOPENPGP=tfc_OPENPGP-tic_OPENPGP
 #print msgen
 
+tid_OPENPGP=time()
 decipher = AES.new(llave,AES.MODE_OPENPGP,iv)		
 decipher.decrypt(msgen)
-tf_OPENPGP=time()
-tiempoOPENPGP=tf_OPENPGP-ti_OPENPGP
-print 'OPENPGP',tiempoOPENPGP
-OPENPGP=['OPENPGP',tiempoOPENPGP]
+tfd_OPENPGP=time()
+tiempodOPENPGP=tfd_OPENPGP-tid_OPENPGP
+print 'OPENPGP Cifrado',tiempocOPENPGP,'Tiempo:', (len(msg))/(tiempocOPENPGP), 'Bytes/s'
+print 'OPENPGP Descifrado',tiempodOPENPGP,'Tiempo:', (len(msg))/(tiempodOPENPGP), 'Bytes/s'
 
-tiempos=[tiempoCBC,tiempoCFB,tiempoCTR,tiempoECB,tiempoOFB,tiempoOPENPGP]
+tiemposc=[tiempocCBC,tiempocCFB,tiempocCTR,tiempocECB,tiempocOFB,tiempocOPENPGP]
+tiemposd=[tiempodCBC,tiempodCFB,tiempodCTR,tiempodECB,tiempodOFB,tiempodOPENPGP]
 nombres=['CBC','CFB','CTR','ECB','OFB','OPENPGP']
-menor=tiempos[0]
+menor=tiemposc[0]
 indice=0
-for valor in tiempos: 
+for valor in tiemposc: 
     if valor < menor: 
         menor = valor
-        indice=tiempos.index(valor)      
+        indice=tiemposc.index(valor)      
 print  nombres[indice] ,menor
+menores=tiemposd[0]
+indices=0
+for valores in tiemposd: 
+    if valores < menor: 
+        menores = valores
+        indices=tiemposd.index(valores)      
+print  nombres[indices] ,menores
